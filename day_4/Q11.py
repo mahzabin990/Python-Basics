cdd4e510620eef0fdc37bb7039c9f8a45b8b75d7
Lira <lirabuet15@gmail.com>
2023-09-15 00:48:28 +0600

    Day 3

--- day_4/Q11.py
+++
@@ -1,7 +0,0 @@
-bands = ['oasis', 'beatles', 'smiths', 'smiths', 'oasis']
-
-x=set(bands)
-print(x)
-
-y=list(x)
-print(y)
--- day_4/Q12.py
+++
@@ -1,8 +0,0 @@
-student = {'name':'Lira','age':'27','courses': ['Math','Compsci'],'status':'married','country':'BD','session':'spring'}
-
-y=[student.items()]
-
-print(y)
-
-
-  
